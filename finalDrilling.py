d43f792c09894f393eabf8bdb158b93b11e1e506
sssjjj222 <95467227+sssjjj222@users.noreply.github.com>
2023-05-10 12:53:52 -0400

    se arregla funcion

--- finalDrilling.py
+++ finalDrilling.py
@@ -2,46 +2,51 @@
 nombres = ["Harry Houdini", "Newton", "David Blaine",
            "Hawking", "Messi", "Teller", "Einstein", "Pele", "Juanes"]
 magos = ["Harry Houdini", "David Blaine", "Teller"]
-magosGrandiosos = []
+magosGrandiosos = magos.copy()
 cientificos = ["Newton", "Hawking", "Einstein"]
 otros = nombres.copy()
 
 for m in magos:
     for o in otros:
         if o == m:
-            otros.remove(o)  # remueve los
+            otros.remove(o)
+            break
 for c in cientificos:
     for o in otros:
         if o == c:
             otros.remove(o)
+            break
 
 
 def hacer_grandioso():
-    for i, m in enumerate(magos):
-        magosGrandiosos.append("El gran "+m)
+    for i in range(len(magosGrandiosos)):
+        magosGrandiosos[i] = "El gran "+magosGrandiosos[i]
 
 
 def imprimir_nombres():
-    print("Nombres: ", end='')
+    print("Nombres sin modificar: ", end='')
     for i in nombres:
         print(i, end=", ")
-    print()
+    print("\n")
+
+
+def imprimir_todos():
+    print("Magos: ", end="")
+    for i in magosGrandiosos:
+        print(i, end=", ")
+    print("")
+
+    print("Cientificos: ", end="")
+    for i in cientificos:
+        print(i, end=", ")
+    print("")
+
+    print("Otros: ", end="")
+    for i in otros:
+        print(i, end=", ")
+    print("")
 
 
 hacer_grandioso()
 imprimir_nombres()
-
-print("Magos Grandiosos: ", end="")
-for i in magosGrandiosos:
-    print(i, end=", ")
-print()
-
-print("Cientificos: ", end="")
-for i in cientificos:
-    print(i, end=", ")
-print()
-
-print("Otros: ", end="")
-for i in otros:
-    print(i, end=", ")
-print()
+imprimir_todos()
